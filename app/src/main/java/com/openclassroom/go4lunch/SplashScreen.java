31bbd2ec4a352e08ffd3c0d61a27f09cd1fd1932
axelrbe <ribeiro.axel.fabien@gmail.com>
2023-05-26 15:39:23 +0200

    Splash screen activity

---
+++ app/src/main/java/com/openclassroom/go4lunch/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.openclassroom.go4lunch;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(SplashScreen.this, MainActivity.class)), 2000);
+    }
+}
